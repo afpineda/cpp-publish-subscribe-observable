fc8bee9ec5543eeefae04052eef0e75671fd3d2a
afpineda <74291754+afpineda@users.noreply.github.com>
2026-02-09 16:35:58 +0100

    Update event_test.cpp

--- test/event_test/event_test.cpp
+++ test/event_test/event_test.cpp
@@ -12,11 +12,17 @@ struct Mock
 {
     bool executed = false;
     inline static bool class_executed = false;
+    inline static int class_executed_counter = 0;
 
     void clear()
     {
         executed = false;
+    }
+
+    static void class_clear()
+    {
         class_executed = false;
+        class_executed_counter = 0;
     }
 
     void member_callback()
@@ -27,6 +33,7 @@ struct Mock
     static void class_callback()
     {
         class_executed = true;
+        class_executed_counter++;
     }
 } mock1, mock2;
 
@@ -69,6 +76,63 @@ void test2()
     assert(!evt.is_subscribed(&Mock::member_callback, &mock2));
 }
 
+void test3()
+{
+    cout << "- Run callbacks -" << endl;
+    event evt;
+    mock1.clear();
+    mock2.clear();
+    Mock::class_clear();
+    assert(!Mock::class_executed);
+    assert(!mock1.executed);
+    assert(!mock2.executed);
+
+    evt.subscribe(&Mock::member_callback, &mock1);
+    evt.subscribe(Mock::class_callback);
+    evt.subscribe(&Mock::member_callback, &mock2);
+    assert(evt.subscribed() == 3);
+
+    evt();
+    assert(Mock::class_executed);
+    assert(mock1.executed);
+    assert(mock2.executed);
+
+    evt.clear();
+    assert(evt.subscribed() == 0);
+}
+
+void test4()
+{
+    cout << "- Subscribe/unsubscribe twice or more times-" << endl;
+    event evt;
+    evt += Mock::class_callback;
+    evt += Mock::class_callback;
+    evt += Mock::class_callback;
+    assert(evt.subscribed() == 1);
+
+    Mock::class_clear();
+    assert(!Mock::class_executed);
+    assert(Mock::class_executed_counter == 0);
+
+    evt();
+    assert(Mock::class_executed_counter == 1);
+
+    evt -= Mock::class_callback;
+    assert(evt.subscribed() == 0);
+}
+
+void test5()
+{
+    cout << "- Unsubscribe not subscribed -" << endl;
+    event evt;
+    assert(evt.subscribed() == 0);
+    assert(!evt.is_subscribed(Mock::class_callback));
+
+    evt -= Mock::class_callback;
+    assert(evt.subscribed() == 0);
+    assert(!evt.is_subscribed(Mock::class_callback));
+}
+
 //------------------------------------------------------------------------------
 // Main
 //------------------------------------------------------------------------------
@@ -77,5 +141,8 @@ int main()
 {
     test1();
     test2();
+    test3();
+    test4();
+    test5();
     return 0;
 }
